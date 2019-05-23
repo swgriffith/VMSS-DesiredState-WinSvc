001e699f2c1ffc63e6c551a4bc04f3659c610f05
Stephen Griffith <stgriffi@microsoft.com>
2019-05-23 11:58:26 -0400

    Updated DSC powershell script to enable download and installation of a windows service.

--- DesiredState.ps1
+++ DesiredState.ps1
@@ -1,21 +1,46 @@
 configuration TestConfig
 {
-    Node IsWebServer
-    {
-        WindowsFeature IIS
-        {
-            Ensure               = 'Present'
-            Name                 = 'Web-Server'
-            IncludeAllSubFeature = $true
-        }
-    }
+    
+    Import-DSCResource -ModuleName xPSDesiredStateConfiguration
 
-    Node NotWebServer
+    Node ServiceHost
     {
-        WindowsFeature IIS
+        #Create the directory where the service will live. You should set as you prefer.
+        File SetupDir
         {
-            Ensure               = 'Absent'
-            Name                 = 'Web-Server'
+            Ensure = "Present"
+            Type = "Directory"
+            DestinationPath = "C:\Setup"
+        }
+
+        #Download the service zip file. You should update the URL and zip file names accordingly.
+        xRemoteFile DownloadPackage 
+        {  
+            DependsOn       = "[File]SetupDir"
+            Uri             = "<Insert SAS URL to Zip file containing service>"
+            DestinationPath = "c:\Setup\DemoService.zip"
+            MatchSource     = $false
+        }
+
+        #Extract the contents of the zip file to the target service directory.
+        Archive ExtractPackage 
+        {
+            DependsOn = '[xRemoteFile]DownloadPackage'
+            Ensure = "Present"  # You can also set Ensure to "Absent"
+            Path = "C:\Setup\DemoService.zip"
+            Destination = "C:\Setup"
+        }
+
+        #Install and start the windows service
+        xService EnsureServiceStarted {
+            DependsOn = '[Archive]ExtractPackage'
+            Ensure = 'Present'
+            Name = 'DemoService'
+            DisplayName = 'DemoService'
+            Description = 'My Demo Service'
+            Path = 'C:\Setup\DemoService.exe'
+            StartupType = 'Automatic'
+            State = 'Running'
         }
     }
 }
